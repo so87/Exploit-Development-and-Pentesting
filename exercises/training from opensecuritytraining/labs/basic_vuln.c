f350ff1340b12cf889dff08b550358c39567e7fd
so87 <so87@evansville.edu>
2018-08-29 22:57:49 -0400

    explored ida some

--- exercises/training from opensecuritytraining/labs/basic_vuln.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main(int argc, char **argv)
-{
-	char buf[64];
-	strcpy(buf,argv[1]);
-}
-
--- exercises/training from opensecuritytraining/shell_shellcode/shellcode_harness.c
+++
@@ -1,7 +0,0 @@
-int main(int argc, char **argv) 
-{
-	int *ret;
-	ret = (int *)&ret + 2;
-	(*ret) = (int)argv[1];
-}
-
